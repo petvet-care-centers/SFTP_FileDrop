8deba56008ab57d05b404dec4990d7a6e2de9293
qallegue <qiuyi.allegue@gmail.com>
2025-02-13 17:01:07 -0500

    Changes

--- SFTP_FileDrop/SFTP_FileDrop/Program.cs
+++ SFTP_FileDrop/SFTP_FileDrop/Program.cs
@@ -1,70 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Globalization;
-using System.Threading.Tasks;
-using CsvHelper;
-using Microsoft.Data.SqlClient;
-using Renci.SshNet;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SFTP_FileDrop.Classes;
+using System;
 using System.IO;
-using Microsoft.Extensions.Primitives;
 
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        Console.WriteLine("Starting application...");
-
-        var builder = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true);
-
-        IConfigurationRoot configuration = builder.Build();
-
-        string? connectionString = configuration.GetConnectionString("connstr");
-        string? query = configuration["SqlSettings:DailyRevQuery"];
-        string? sftpUsername = configuration["AppSettings:SftpUsername"];
-        string? sftpPassword = configuration["AppSettings:SftpPassword"];
-        string? sftpHost = configuration["AppSettings:SftpHost"];
-        string? sftpFolderPath = configuration["AppSettings:SftpFolderPath"];
-        string formattedDate = DateTime.Now.ToString("MMddyyyy");
-        string localTestFile = "testfile.txt";
-        string? sftpFilePath = sftpFolderPath + "/" + localTestFile; //$"/SalesTransaction {formattedDate}.csv";
-        
-
-
-        //File.WriteAllText(localTestFile, "this is a test file.");
-        try
-        {
-            UploadToSftp(localTestFile, sftpHost, sftpUsername, sftpPassword, sftpFilePath);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-
-        }
-
-        static void UploadToSftp(string localFilePath, string host, string username, string password, string remotePath)
-        {
-            using (SftpClient client = new(host, username, password))
-            {
-                client.Connect();
-
-                Console.WriteLine("Connected to SFTP server");
-
-                using (FileStream fs = new(localFilePath, FileMode.Open))
-                {
-                    client.UploadFile(fs, remotePath);
-                }
-
-                Console.WriteLine("File uploaded successfully");
-
-                client.Disconnect();
-            }
-        }
-        
-
+        var host = CreateHostBuilder(args).Build();
+        var csvFileGeneration = host.Services.GetRequiredService<CsvFileGeneration>();
+        csvFileGeneration.Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                config.SetBasePath(Directory.GetCurrentDirectory());
+                config.AddJsonFile("appSettings.json", optional: true, reloadOnChange: true);
+            })
+            .ConfigureServices((context, services) =>
+            {
+                services.AddSingleton<CsvFileGeneration>();
+            });
 }
